5103ae1babb228217c8a9db9e2c939430831e339
Mr Violets <140274934+MrViolets@users.noreply.github.com>
2023-08-30 19:05:04 +0100

    Add css minification (#4)
    * Add css minification to build
    
    * Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ const imagemin = require('gulp-imagemin')
 const jsonEditor = require('gulp-json-editor')
 const { exec } = require('child_process')
 const uglify = require('gulp-uglify')
-const fs = require('fs')
+const cleanCSS = require('gulp-clean-css')
+const fs = require('fs/promises')
 
 colors.setTheme({
   error: 'red'
@@ -47,7 +48,13 @@ gulp.task('uglify-js', function () {
     .pipe(gulp.dest('build/temp'))
 })
 
-gulp.task('build-chrome', gulp.series('update-version', 'uglify-js', function () {
+gulp.task('minify-css', function () {
+  return gulp.src('src/**/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('build/temp'))
+})
+
+gulp.task('build-chrome', gulp.series('update-version', 'uglify-js', 'minify-css', function () {
   const manifestPath = path.join(__dirname, 'src', 'manifest.json')
   const manifest = require(manifestPath)
   const version = manifest.version
@@ -56,31 +63,9 @@ gulp.task('build-chrome', gulp.series('update-version', 'uglify-js', function ()
     .pipe(imagemin([imagemin.optipng({ optimizationLevel: 5 })]))
     .pipe(zip(`${pkg.name}-v${version}.zip`))
     .pipe(gulp.dest('build'))
-    .on('end', function () {
-      fs.promises.rm('build/temp', { recursive: true, force: true })
+    .on('end', async function () {
+      await fs.rm('build/temp', { recursive: true, force: true })
     })
 }))
-
-gulp.task('update-version', function (cb) {
-  const manifestPath = path.join(__dirname, 'src', 'manifest.json')
-  const manifest = require(manifestPath)
-  const version = manifest.version
-
-  return gulp.src('./package.json')
-    .pipe(jsonEditor({ version }))
-    .pipe(gulp.dest('./'))
-    .on('end', function () {
-      exec('npm install', { cwd: './' }, function (error) {
-        if (error) {
-          logMessage(colors.error('Error running npm install: ' + error))
-          return
-        }
-        cb()
-      })
-    })
-    .on('error', function (err) {
-      logMessage(colors.error('Error updating version in package.json: ' + err.toString()))
-    })
-})
 
 gulp.task('build', gulp.series('update-version', 'build-chrome'))
